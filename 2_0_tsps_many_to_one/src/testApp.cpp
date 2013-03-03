cf4025e26cf48db505442f8a3c65601bb32d6387
Carl Jamilkowski <cdj255@nyu.edu>
2013-03-03 13:50:20 -0500

    Removing extra code

--- 2_0_tsps_many_to_one/src/testApp.cpp
+++ 2_0_tsps_many_to_one/src/testApp.cpp
@@ -10,12 +10,6 @@ void testApp::setup(){
     ofBackground(0);
     ofSetFrameRate(60);
     ofSetCircleResolution(60);
-
-}
-
-//--------------------------------------------------------------
-void testApp::update(){
-
 }
 
 //--------------------------------------------------------------
@@ -42,45 +36,4 @@ void testApp::draw(){
         
         ofCircle(centroid, radius);
     }
-}
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
